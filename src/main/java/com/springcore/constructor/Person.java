06fe96d764e3b64db1a6c64c9291cbb86cd7c31e
aartinotani2002 <aarti.notani2002@gmail.com>
2026-03-09 19:25:17 +0530

    Initial commit of springcore project

---
+++ src/main/java/com/springcore/constructor/Person.java
@@ -0,0 +1,22 @@
+package com.springcore.constructor;
+
+public class Person {
+	private String name;
+	private int personId;
+	private Certificate certificate;
+	
+	public Person(String name , int personId,Certificate certificate) {
+		this.name = name;
+		this.personId = personId;
+		this.certificate=certificate;
+	}
+
+	@Override
+	public String toString() {
+		return this.name+":"+ this.personId+"{"+this.certificate.name+"}";
+	}
+	
+	
+	
+
+}
